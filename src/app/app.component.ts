70d0dc572e38c479182a85af79462e60a9241db2
ElitLien <supervlad586@gmail.com>
2023-10-12 23:50:58 +0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'USENSE_Test_Task';
+  password: string = '';
+  sectionColors = ['gray', 'gray', 'gray'];
+
+  checkPasswordStrength() {
+    if (!this.password) {
+      this.sectionColors = ['gray', 'gray', 'gray'];
+      return;
+    }
+    if (this.password.length < 8) {
+      this.sectionColors = ['red', 'red', 'red'];
+      return;
+    }
+    const hasLetters = /[a-zA-Z]/.test(this.password);
+    const hasNumbers = /\d/.test(this.password);
+    const hasSymbols = /\W|_/.test(this.password);
+    if (hasLetters && hasNumbers && hasSymbols) {
+      this.sectionColors = ['green', 'green', 'green'];
+    } else if ((hasLetters && hasNumbers) || (hasLetters && hasSymbols) || (hasNumbers && hasSymbols)) {
+      this.sectionColors = ['yellow', 'yellow', 'gray'];
+    } else {
+      this.sectionColors = ['red', 'gray', 'gray'];
+    }
+  }
 }
